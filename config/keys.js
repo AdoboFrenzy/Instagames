0f02a7dea9dd91a50ec2067c6d444d41dc4556c3
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-10-02 09:27:24 -0700

    updates

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
+
     mongoURI: 
     'mongodb://datrupnoy:alfred123@ds018568.mlab.com:18568/cleaningapp',
     secretOrKey: 'secret'
+    
 };
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = new Schema({
+
     name: {
         type: String,
         required: true
@@ -23,6 +24,7 @@ const UserSchema = new Schema({
         type: Date,
         dafault: Date.now
     }
+    
 });
 
 module.exports = User = mongoose.model('users', UserSchema);
