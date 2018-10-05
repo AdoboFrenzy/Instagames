dbe4a671deb0a17dd0915a9164c6e4ae58292d1c
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-10-05 09:32:17 -0700

    begin implementing actions

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root_reducer';
+// import logger from 'redux-logger';
 
+const preloadedState = {}
 const middleware = [thunk];
 
-const configureStore = (preloadedState = {}) => (
+const configureStore = (
     createStore(
         rootReducer, 
-        preloadedState, 
-        applyMiddleware(...middleware))
+        preloadedState,
+        compose(
+            applyMiddleware(...middleware)),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+        
 )
 
 export default configureStore;
