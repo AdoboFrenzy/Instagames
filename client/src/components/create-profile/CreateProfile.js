d99d65a6e082415c3258efc133af20f5a7986b08
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-10-07 12:05:29 -0700

    add text area groups to create profile page

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -28,9 +28,34 @@ class CreateProfile extends React.Component {
             instagram: '',
             errors: {}
         }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        console.log('submit');
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
     }
     
     render() {
+        const { errors } = this.state;
+
+        // Select options for status
+        const options = [
+            { label: '* Select Type', value: 0 },
+            { label: 'Casual', value: 'Casual' },
+            { label: 'Competitive', value: 'Competitive' },
+            { label: 'Professional', value: 'Professional' },
+            { label: 'Coach', value: 'Coach' },
+            { label: 'Other', value: 'Other' }
+        ];
+
         return (
             <div className="create-profile">
                 <div className="container">
@@ -41,6 +66,75 @@ class CreateProfile extends React.Component {
                                 Let's get some information to make your profile stand out
                             </p>
                             <small className="d-block pb-3">* = required fields</small>
+                            <form onSubmit={this.onSubmit}>
+                                <TextFieldGroup 
+                                    placeholder="* Profile Handle"
+                                    name="handle"
+                                    value={this.setState.handle}
+                                    onChange={this.onChange}
+                                    error={errors.handle}
+                                    info="A unique handle for your profile URL. Full name, company name, nick name, etc"
+                                />
+                                <SelectListGroup
+                                    placeholder="* Status"
+                                    name="status"
+                                    value={this.setState.status}
+                                    onChange={this.onChange}
+                                    options={options}
+                                    error={errors.status}
+                                    info="Give us an idea of what type of gamer you are"
+                                />
+                                <TextFieldGroup
+                                    placeholder="Team or Company"
+                                    name="company"
+                                    value={this.setState.company}
+                                    onChange={this.onChange}
+                                    error={errors.company}
+                                    info="A team or company that sponsors you or N/A"
+                                />
+                                <TextFieldGroup
+                                    placeholder="Location"
+                                    name="location"
+                                    value={this.setState.location}
+                                    onChange={this.onChange}
+                                    error={errors.location}
+                                    info="City and State"
+                                />
+                                <TextFieldGroup
+                                    placeholder="* Game Types"
+                                    name="skills"
+                                    value={this.setState.skills}
+                                    onChange={this.onChange}
+                                    error={errors.skills}
+                                    info="Types of games you play (ex. RPG, Strategy, Boardgames, First Person Shooters)"
+                                />
+                                <TextFieldGroup
+                                    placeholder="Twitch or Discord Username"
+                                    name="githubusername"
+                                    value={this.setState.githubusername}
+                                    onChange={this.onChange}
+                                    error={errors.githubusername}
+                                    info="Twitch or Discord Username - so people can contact you"
+                                />
+                                <TextAreaFieldGroup
+                                    placeholder="Short Bio"
+                                    name="bio"
+                                    value={this.setState.bio}
+                                    onChange={this.onChange}
+                                    error={errors.bio}
+                                    info="Tell us about yourself"
+                                />
+                                <div className="mb-3">
+                                    <button onClick={() => {
+                                        this.setState(prevState => ({
+                                            displaySocialInputs: !prevState.displaySocialInputs
+                                        }))
+                                    }}>
+                                        Add Social Network Links
+                                    </button>
+                                    <span className="text-muted">Optional</span>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
