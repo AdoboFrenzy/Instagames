2e3c55f6c006be9ebf313e3ecec33c6ee7e08b1a
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-10-06 19:34:17 -0700

    create textfieldgroup and factor out code from components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { mapStateToProps, mapDispatchToProps } from './LoginContainer';
 // import { loginUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup'
 
 class Login extends React.Component {
     constructor() {
@@ -63,36 +64,24 @@ class Login extends React.Component {
                             <h1 className="display-4 text-center">Log In</h1>
                             <p className="lead text-center">Sign in to your Instagames account</p>
                             <form onSubmit={this.onSubmit}>
-                                <div className="form-group">
-                                    <input 
-                                        type="email" 
-                                        className={classnames('form-control form-control-lg', {
-                                            'is-invalid': errors.email
-                                        })}
-                                        placeholder="Email Address" 
-                                        name="email" 
-                                        value={this.state.email}
-                                        onChange={this.onChange}
-                                        />
-                                        {errors.email && (
-                                            <div className="invalid-feedback">{errors.email}</div>
-                                        )}
-                                </div>
-                                <div className="form-group">
-                                    <input 
-                                    type="password" 
-                                    className={classnames('form-control form-control-lg', {
-                                        'is-invalid': errors.password
-                                    })}
-                                    placeholder="Password" 
-                                    name="password" 
+                                <TextFieldGroup 
+                                    placeholder="Email Address"
+                                    name="email"
+                                    type="email"
+                                    value={this.state.email}
+                                    onChange={this.onChange}
+                                    error={errors.email}
+                                />
+
+                                <TextFieldGroup
+                                    placeholder="Password"
+                                    name="password"
+                                    type="password"
                                     value={this.state.password}
                                     onChange={this.onChange}
-                                    />
-                                    {errors.password && (
-                                        <div className="invalid-feedback">{errors.password}</div>
-                                    )}
-                                </div>
+                                    error={errors.password}
+                                />
+
                                 <input type="submit" className="btn btn-info btn-block mt-4" />
                             </form>
                         </div>
