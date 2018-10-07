eb0f79d6b80ce152f7240da64e933d8666921945
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-10-06 19:40:29 -0700

    create textfieldgroup and factor out code from components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { mapStateToProps, mapDispatchToProps } from './LoginContainer';
-// import { loginUser } from '../../actions/authActions';
 import TextFieldGroup from '../common/TextFieldGroup'
 
 class Login extends React.Component {
