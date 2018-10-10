51cbfa86094e679f9296e5926168d53dd0924175
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-10-09 18:55:09 -0700

    single post display almost done.. begin work on comment form on single page post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Spinner from '../common/spinner';
+import PostItem from '../posts/PostItem';
 import { mapStateToProps, mapDispatchToProps } from './PostContainer';
 
 class Post extends React.Component {
@@ -11,9 +13,32 @@ class Post extends React.Component {
     }
 
   render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if(post === null || loading || Object.keys(post).length === 0) {
+        postContent = <Spinner />
+    } else {
+        postContent = (
+            <div>
+                <PostItem post={post} showActions={false} />
+            </div>
+        );
+    }
+
     return (
-      <div>
-        <h1>POST</h1>
+      <div className="post">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                        <Link to="/feed" className="btn btn-light mb-3">
+                            Back to Feed
+                        </Link>
+                        {postContent}
+                </div>
+            </div>
+
+        </div>
       </div>
     )
   }
